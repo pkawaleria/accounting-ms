5e084a86db0ca416398c9fb6d33b5bce4b048569
Kamilek <104760585+KlonicaRadoslaw@users.noreply.github.com>
2023-11-06 16:42:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode, that will refresh the page when you make changes.
 DEBUG = True
 
-# DB_USER = 'root'
-# DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'password'
-# DB_HOST = 'db'
-# DB_NAME = 'users'
+DB_USER = 'root'
+DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'password'
+DB_HOST = 'db'
+DB_NAME = 'users'
 
 JWT_SECRET = 'LRFYY4iEwYOYmU5E9xL9hUOQKiAdlJ1234odfdacp213af2favma2rfav24favlacsaa3agvdaasdfataeg3qtqgter3qrt32tdgsadfgacdsbv34t32tr4afafawfasfasf'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/inz'
-# SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:{DB_PASSWORD}@db/{DB_NAME}'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/inz'
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
